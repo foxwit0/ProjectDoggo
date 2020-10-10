1e14b7921c59c3470d75edbfc7eb028a118258d6
DarkSpirale <71567838+DarkSpirale@users.noreply.github.com>
2020-10-10 18:38:31 +0200

    BaseCommit

---
+++ ProjectDoggo Unity/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    //[SerializeField] private GameObject player;
+    //[SerializeField] private float timeOffset;
+    //[SerializeField] private Vector3 posOffset;
+
+    public GameObject player;
+    public float timeOffset;
+    public Vector3 posOffset;
+
+    private Vector3 velocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.SmoothDamp(transform.position, player.transform.position + posOffset, ref velocity, timeOffset);
+    }
+}

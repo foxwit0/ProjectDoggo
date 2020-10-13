b044f6feaa327dc27cd3b9b2d7a22fa704dbaa72
foxwit0 <foxwit.0@gmail.com>
2020-10-13 15:21:34 +0200

    Nouveau Plasma Violet - Anime éclaire - SpeedOffset

--- ProjectDoggo Unity/Assets/Scripts/PlasmaMovement.cs
+++ ProjectDoggo Unity/Assets/Scripts/PlasmaMovement.cs
@@ -3,19 +3,25 @@
 public class PlasmaMovement : MonoBehaviour
 {   
     private float fallSpeed;
+    public float fallSpeedOffset = -100f;
+    public float horizontalSpeed = 100f;
+    public float maxHorizontalBalance = 2f;
 
     [HideInInspector] public Rigidbody2D rb;
-
+    
     private void Start() 
     {
         rb = transform.GetComponent<Rigidbody2D>();
 
-        fallSpeed = PlayerMovement.instance.fallSpeed - 100f;
+        fallSpeed = PlayerMovement.instance.fallSpeed + fallSpeedOffset;
     }
 
     private void FixedUpdate() 
     {
-        Vector3 targetVelocity = new Vector2(0f, -1f * fallSpeed); // Le -1 permet d'avoir un mouvement vers le bas
+        if(rb.position.x >= maxHorizontalBalance) horizontalSpeed = -horizontalSpeed;
+        if(rb.position.x <= -maxHorizontalBalance) horizontalSpeed = -horizontalSpeed;
+        
+        Vector3 targetVelocity = new Vector2(horizontalSpeed, -1f * fallSpeed); // Le -1 permet d'avoir un mouvement vers le bas
 
         rb.velocity = targetVelocity * Time.fixedDeltaTime;
     }

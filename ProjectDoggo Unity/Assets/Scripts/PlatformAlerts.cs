87d2ec68fe0786d3810f6facdf7c1abcef11fe29
DarkSpirale <71567838+DarkSpirale@users.noreply.github.com>
2020-10-14 21:05:09 +0200

    Intégration de l'affichage et disparition des alertes plateforme
    Gestion d'affichage/masquage des alertes plateforme opérationnel.
    Il faut encore tester différentes configurations de plateformes et en mêlant différents colliders pour vérifier qu'il n'y a pas de bug.
    Reste à gérer le changement de couleur (ou autre) en fonction de la distance de la plateforme, + éventuels effets à l'apparition/disparition

--- ProjectDoggo Unity/Assets/Scripts/PlatformAlerts.cs
+++ ProjectDoggo Unity/Assets/Scripts/PlatformAlerts.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlatformAlerts : MonoBehaviour
 {
@@ -9,28 +10,56 @@ public class PlatformAlerts : MonoBehaviour
     [SerializeField] private float startDetectionPosOffset = -18f;
     [SerializeField] private float detectionDistance = 30f;
 
+    [SerializeField] private GameObject platformAlertsGO = null;
+    public Image[] platformAlerts;
+
     void Start()
     {
         player = PlayerMovement.instance;
+
+        //Récupération des GO de chaque alerte plateforme
+        platformAlerts = platformAlertsGO.GetComponentsInChildren<Image>();
     }
 
     void Update()
     {
         playerYPosition = player.transform.position.y;        
 
-        for(int i = 0; i <= 16; i++)
+        for(int i = 0; i < platformAlerts.Length; i++)
         {
-            Vector2 raycastStartPos = new Vector2(i - 8f, playerYPosition + startDetectionPosOffset);
+            Vector2 raycastStartPos = new Vector2(i - (platformAlerts.Length / 2f) + 0.5f, playerYPosition + startDetectionPosOffset);
             RaycastHit2D hit = Physics2D.Raycast(raycastStartPos, Vector2.down, detectionDistance);
+            Debug.DrawRay(raycastStartPos, Vector2.down * detectionDistance, Color.red);
 
             if(hit)
             {
                 if(hit.collider.CompareTag("Platform"))
                 {
                     //Debug.Log("Collision position x : " + hit.point.x);
+                    ShowAlert(i);
+                }
+                else
+                {
+                    if(platformAlerts[i].enabled)
+                        HideAlert(i);
                 }
             } 
-
+            else
+            {
+                if(platformAlerts[i].enabled)
+                    HideAlert(i);
+            }
         }
     }
+
+    private void ShowAlert(int alertIndex)
+    {
+        platformAlerts[alertIndex].enabled = true;
+    }
+
+    private void HideAlert(int alertIndex)
+    {
+        platformAlerts[alertIndex].enabled = false;
+    }
+
 }

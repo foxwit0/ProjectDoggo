5ff1fbb9915441cd2dc43257166c8db276dfea18
DarkSpirale <71567838+DarkSpirale@users.noreply.github.com>
2020-10-21 22:40:07 +0200

    Ajout de la barre de progression dans le niveau
    Ajout de la barre de progression dans le niveau :
    - Ajout d'un GO "LevelManager" pour renseigner la taille du niveau + script de la barre de progression + script des alertes plateformes. Peut être étendu à d'autres usages
    - Ajout de la barre de progression dans le RightPanel du canvas.
    - Le déplacement de la position du joueur est actuellement gérée. Reste le plasma à ajouter.

---
+++ ProjectDoggo Unity/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public float levelHeight;   
+
+    #region Singleton
+    public static LevelManager instance;
+
+    void Awake()
+    {
+        if(instance!= null)
+        {
+            Debug.LogError("Il y a plus d'une instance de LevelManager en jeu") ;
+            return;
+        }
+        instance = this;
+    }
+    #endregion Singleton
+}
---
+++ ProjectDoggo Unity/Assets/Scripts/LevelProgressBar.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelProgressBar : MonoBehaviour
+{
+    [SerializeField] private RectTransform progressBarUI = null;
+    [SerializeField] private RectTransform playerUI = null;
+    [SerializeField] private GameObject plasmaUI = null;
+
+    private float levelHeight;
+    private float progressBarHeight;
+    private Image plasma;
+
+    private Transform playerPosGO;
+
+    void Start()
+    {
+        levelHeight = LevelManager.instance.levelHeight;
+
+        Image progressBar = progressBarUI.GetComponent<Image>();
+        progressBarHeight = progressBarUI.sizeDelta.y;
+
+        plasma = plasmaUI.GetComponent<Image>();
+
+        playerPosGO = PlayerMovement.instance.transform;
+    }
+
+    void Update()
+    {
+        //Rafraichissement de la position du joueur dans la barre de progression
+        float newPlayerPosUI = Mathf.Clamp((playerPosGO.position.y / levelHeight) * progressBarHeight, -progressBarHeight + playerUI.sizeDelta.y / 2f, 0f);
+        playerUI.anchoredPosition = new Vector2(0f, newPlayerPosUI);
+
+    }
+}

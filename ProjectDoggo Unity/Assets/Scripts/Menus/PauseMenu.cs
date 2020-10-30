2eb12764ef6663286ebcd54e79a67fe73101ecd5
foxwit0 <foxwit.0@gmail.com>
2020-10-30 13:00:20 +0100

    Ajout du menu pause
    Il a y les boutons
    -Continuer
    -Recommencer
    -Options
    -Menu principal
    -Quitter
    
    Note : La préfab du menu options est intégré et fonction correctement mais je souhaiterai pouvoir mettre le même background que dans le menu pause. Je ne peux pas le faire sans modifier le rendu du main menu, à moins de faire un truc inutilement compliqué. De toute manière je pense que le menu pause va devenir une préfab en soi donc l'intérêt d'intégrer la préfab menu options est peut être limité. A voir.

---
+++ ProjectDoggo Unity/Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public void PlayTime()
+    {
+        //Jouer le temps
+        Time.timeScale = 1;
+    }
+
+    public void Restart()
+    {
+        PlayTime();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void MainMenu()
+    {
+        PlayTime();
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void QuitGame()
+    {
+        Debug.Log("QUIT !!");
+        Application.Quit();
+    }
+}
---
+++ ProjectDoggo Unity/Assets/Scripts/Menus/UI.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UI : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    void Update()
+    {
+        //Ouvrir le menu pause avec la touche echap
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            pauseMenu.SetActive(true);
+
+            //Arrêt du temps
+            Time.timeScale = 0;
+        }
+    }
+}

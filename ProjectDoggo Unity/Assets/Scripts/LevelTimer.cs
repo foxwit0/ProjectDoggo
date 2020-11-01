396abf8f18bd77c096b75cccf9517b53e396b1ef
foxwit0 <foxwit.0@gmail.com>
2020-11-01 13:54:24 +0100

    Ajout Timer Niveau
    J'ai ajouté la fonction du timer avec l'UI associé. J'ai essayé de faire une séparation entre script UI et script "process".
    La fonction d'arrêt du chrono lors du menu pause ou mort se fait toute seule de manière implicite car on met le timescale à 0.
    Le reset du timer se fait aussi de manière implicite parce qu'on recharge le niveau.
    J'ai aussi importer un nouvelle font monospaced pour ne pas avoir un tremblement du compteur quand il défile.

---
+++ ProjectDoggo Unity/Assets/Scripts/LevelTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelTimer : MonoBehaviour
+{
+    [SerializeField] private TimerUI timerUI = null;
+    private float startTime;
+    private float currentTimer;
+
+    private void Start()
+    {
+        if(timerUI == null)
+            Debug.LogWarning("Missing TimerUI object in inspector of LevelTimer.cs");
+        startTime = Time.time;
+    }
+
+    private void Update()
+    {
+        currentTimer = Time.time - startTime;
+        timerUI.UpdateTimer(currentTimer);
+    }
+
+    public void Reset()
+    {
+        startTime = Time.time;
+    }
+}
---
+++ ProjectDoggo Unity/Assets/Scripts/TimerUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimerUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textMesh = null;
+
+    void Start() 
+    {
+        if(textMesh == null)
+            Debug.LogWarning("Missing TMP_Text object in inspector of TimerUI.cs");
+        textMesh.text = CounterToTextTimer(0f);
+    }
+    
+    public void UpdateTimer(float counter) //Compteur en secondes
+    {
+        textMesh.text = CounterToTextTimer(counter);
+    }
+
+    private string CounterToTextTimer(float counter)
+    {
+        float seconds = counter % 60;
+        int minutes = (int) (counter / 60);
+
+        return (minutes*10000 + seconds*100).ToString("00:00:00");
+    }
+
+}

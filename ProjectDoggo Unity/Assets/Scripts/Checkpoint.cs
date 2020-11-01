def9e217d0b0f2de4c8180db18aedfa7c88dec4f
DarkSpirale <71567838+DarkSpirale@users.noreply.github.com>
2020-11-01 21:10:25 +0100

    Affichage checkpoint
    Effet de lineRenderer pour représenter le checkpoint

--- ProjectDoggo Unity/Assets/Scripts/Checkpoint.cs
+++ ProjectDoggo Unity/Assets/Scripts/Checkpoint.cs
@@ -6,12 +6,40 @@ public class Checkpoint : MonoBehaviour
     [SerializeField] private RespawnSystem respawnSystem = null;
     [SerializeField] private GameObject plasma = null;
 
+    private LineRenderer lineRenderer;
+    [Header("Width Variation")]
+    [SerializeField] float widthVariationPerUpdate = 0.005f;
+    [SerializeField] float maxWidthVariation = 0.25f;
+    private float startWidth;
+    private bool widthExpanding = true;
+
     void Start() 
     {
         if(respawnSystem == null)
             Debug.LogWarning("Missing RespawnSystem object in inspector of Checkpoint.cs");
         if(plasma == null)
             Debug.LogWarning("Missing plasma object in inspector of Checkpoint.cs");
+
+        lineRenderer = transform.GetComponent<LineRenderer>();
+        startWidth = lineRenderer.startWidth;
+    }
+
+    void Update()
+    {
+        //Variation de l'épaisseur du checkpoint
+        if(widthExpanding)
+        {
+            lineRenderer.startWidth += widthVariationPerUpdate;
+            if(lineRenderer.startWidth >= startWidth + maxWidthVariation)
+                widthExpanding = false;
+        }
+        else
+        {
+            lineRenderer.startWidth -= widthVariationPerUpdate;
+            if(lineRenderer.startWidth <= startWidth)
+                widthExpanding = true;
+        }
+        
     }
 
     void OnTriggerEnter2D(Collider2D other) 
